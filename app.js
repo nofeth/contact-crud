f7b6819b127ce823e5b5eb7e79b92b3be3ef3338
Ade Oktaviano Arrahman <adeoktaviano50@gmail.com>
2022-08-21 08:11:35 +0700

    add validator

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-import { saveContact,question } from "./contacts.js"
+import yargs from "yargs"
+import { hideBin } from 'yargs/helpers'
+import { saveContact } from "./contacts.js"
+import fs from 'fs'
+yargs(hideBin(process.argv)).command(
+    'add',
+    'Masukkan Data',
+    function(yargs){
+        return yargs.options({
+            'nama': {
+                describe : "Nama Lengkap",
+                demandOption : true,
+                type : 'string',
+            },
+            'email'  : {
+                describe : 'Email Lengkap',
+                demandOption : false,
+                type : 'string'
+            },
+            'noHp' : {
+                describe : 'Nomor Handphone',
+                demandOption : true,
+                type : 'string'
+            }
 
-const main = async () => {
-    const nama = await question("Nama Kamu : ")
-    const email = await question("Email Kamu : ")
-    const noHp = await question("No Hp Kamu : ")
-    saveContact(nama,email,noHp)
-}
+            })},
+    function({nama,email,noHp}){
+        saveContact(nama,email,noHp)
+    } 
+).parse()
 
-main()
+
+
+// import { saveContact,question } from "./contacts.js"
+// const main = async () => {
+//     const nama = await question("Nama Kamu : ")
+//     const email = await question("Email Kamu : ")
+//     const noHp = await question("No Hp Kamu : ")
+//     saveContact(nama,email,noHp)
+// }
+
+// main()
